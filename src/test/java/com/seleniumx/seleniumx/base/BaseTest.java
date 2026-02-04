bb393b19ac75685a84cc264e68546398aff7310b
Manjitha Muthugala <nmmuthugala@gmail.com>
2026-02-04 09:58:09 +0530

    Updated script according to code analysis

--- src/test/java/com/seleniumx/seleniumx/base/BaseTest.java
+++ src/test/java/com/seleniumx/seleniumx/base/BaseTest.java
@@ -1,16 +1,13 @@
 package com.seleniumx.seleniumx.base;
 
-import function.Home;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
 
     protected WebDriver driver;
-    Home home;
 
     @BeforeClass
     public void setDriver(){
--- src/test/java/com/seleniumx/seleniumx/tests/siteNavigationTest.java
+++ src/test/java/com/seleniumx/seleniumx/tests/siteNavigationTest.java
@@ -1,10 +1,6 @@
 package com.seleniumx.seleniumx.tests;
 
 import com.seleniumx.seleniumx.base.BaseTest;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import function.Home;
 
